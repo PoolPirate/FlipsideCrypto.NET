ce36946258b45cff40443e7e2e2ae91e4cf696f5
Playwo <eliaswolf1609@gmail.com>
2023-12-20 00:31:52 +0100

    fix: Propertly typed nullability

--- src/Models/JsonRPC/GetQueryRunResults/GetQueryRunResultsResult.cs
+++ src/Models/JsonRPC/GetQueryRunResults/GetQueryRunResultsResult.cs
@@ -7,7 +7,7 @@ internal class GetQueryRunCSVResultsResult : IJsonRPCResult
     public required string[] ColumnNames { get; init; }
     public required string[] ColumnTypes { get; init; }
 
-    public required JsonNode[][] Rows { get; init; }
+    public required JsonNode?[][] Rows { get; init; }
 
     public required QueryPagination Page { get; init; }
     public required QueryRun OriginalQueryRun { get; init; }
